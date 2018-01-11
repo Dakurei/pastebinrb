fc7757539a1315f0ded39ffa8912cadad9f59714
Maxime Palanchini <m.palanchini@laposte.net>
2018-01-11 16:11:19 +0100

    Création des méthodes par défaut pour générer des pastebins ou pour en lire

--- lib/pastebinrb.rb
+++ lib/pastebinrb.rb
@@ -1,5 +1,31 @@
 require "pastebinrb/version"
+require "pastebinrb/expiration"
+require 'net/http'
 
 module Pastebinrb
-  # Your code goes here...
+  class Pastebin
+    BASE_URL = "https://pastebin.com"
+
+    attr_reader :devkey
+
+    def initialize( devkey )
+      @devkey = devkey
+    end
+
+    def paste_content( content, **options )
+      params = {
+        :api_dev_key => @devkey,
+        :api_option => "paste",
+        :api_paste_name => options[:name],
+        :api_paste_format => options[:format],
+        :api_paste_expire_date => options[:expire_date],
+        :api_paste_code => content
+      }
+      Net::HTTP.post_form(URI.parse("#{BASE_URL}/api/api_post.php"), params).body
+    end
+
+    def get_raw(link)
+      Net::HTTP.get_response(URI.parse("#{BASE_URL}/raw/#{link[/[\w\d]+$/]}")).body
+    end
+  end
 end
