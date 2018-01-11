a0b48587b85e07a8ef36a0aaf790c015bfbc8155
Maxime Palanchini <m.palanchini@laposte.net>
2018-01-11 16:15:54 +0100

    Création d'un module avec les constantes pour l'expiration du fichier

---
+++ lib/pastebinrb/expiration.rb
@@ -0,0 +1,13 @@
+module Pastebinrb
+  module Expiration
+    NEVER = 'N'
+    MINUTE_10 = "10M"
+    HOUR_1 = "1H"
+    DAY_1 = "1D"
+    WEEK_1 = "1W"
+    WEEK_2 = "2W"
+    MONTH_1 = "1M"
+    MONTH_6 = "6M"
+    YEAR_1 = "1Y"
+  end
+end
